a9de750802217fbd31d9e0da24f38300563e79d0
ywseo <ywseo@autocrypto.io>
2023-03-01 01:44:04 +0900

    10799 metal rod

---
+++ p2583.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Baekjoon {
+	class Program {
+		static void Main() {
+			string s = Console.ReadLine();
+			string[] ss = s.Split();
+			int a = int.Parse(ss[0]);
+			int b = int.Parse(ss[1]);
+			Console.WriteLine(a+b);
+		}
+	}
+}
