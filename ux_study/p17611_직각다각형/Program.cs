d8ede9235746f4064cf0d8c2630867448b2b98f4
ywseo <syw31023@naver.com>
2023-03-19 16:35:12 +0900

    17611 직각다각형 해결
    x, y축 각각의 최대 배열을 만들어 놓고
    선이 한번 지날때마다 이에 대항되는 좌표값들을 1씩 증가시킴.
    해당 좌표에 선이 10번 지나간다면 값이 10이 되어 있을 것이므로
    
    n까지 진행한 후에 0이 아닌 x, y 위치의 값들 중에서 최대값을 찾으면 됨

---
+++ ux_study/p17611_직각다각형/Program.cs
@@ -0,0 +1,164 @@
+﻿#define SOLVE
+
+//https://www.acmicpc.net/problem/17611
+
+
+
+#if SOLVE 
+
+using System.Drawing;
+
+
+List<Point> arr = new List<Point>();
+
+string input = Console.ReadLine();
+int n = 0;
+int.TryParse(input, out n);
+
+for (int i = 0; i < n; i++)
+{
+    input = Console.ReadLine();
+    string[] words = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    arr.Add(new Point(int.Parse(words[0]), int.Parse(words[1])));
+}
+
+int[] arr_x = new int[1000001];
+int[] arr_y = new int[1000001];
+
+Point pt_curr = new Point();
+Point pt_prev = new Point(arr.Last().X, arr.Last().Y);
+for (int i = 0; i < n; i++)
+{
+    pt_curr.X = arr[i].X;
+    pt_curr.Y = arr[i].Y;
+
+    if (pt_prev.Y != pt_curr.Y)
+    {
+        // y축으로 이동
+        int min_y = Math.Min(pt_prev.Y, pt_curr.Y);
+        int max_y = Math.Max(pt_prev.Y, pt_curr.Y);
+        for (int k = min_y; k < max_y; k++)
+        {
+            arr_y[k + 500000]++;
+        }
+    }
+    if (pt_prev.X != pt_curr.X)
+    {
+        // x축으로 이동
+        int min_x = Math.Min(pt_prev.X, pt_curr.X);
+        int max_x = Math.Max(pt_prev.X, pt_curr.X);
+        for (int k = min_x; k < max_x; k++)
+        {
+            arr_x[k + 500000]++;
+        }
+    }
+    pt_prev.X = pt_curr.X;
+    pt_prev.Y = pt_curr.Y;
+}
+Console.WriteLine(Math.Max(arr_x.Max(), arr_y.Max()));
+
+
+#else 
+
+
+using System.Drawing;
+
+
+List<Point> arr = new List<Point>();
+
+string input = Console.ReadLine();
+int n = 0;
+int.TryParse(input, out n);
+
+for (int i = 0; i < n; i++)
+{
+    input = Console.ReadLine();
+    string[] words = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    try
+    {
+        arr.Add(new Point(int.Parse(words[0]), int.Parse(words[1])));
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+
+Console.WriteLine("n: {0}", n);
+foreach (Point pt in arr)
+{
+    Console.WriteLine("{0}", pt);
+}
+
+arr.Add(arr[0]);    // 다시 최초 시작점으로 이어져야 하므로
+
+int[] arr_x = new int[1000000];
+int[] arr_y = new int[1000000];
+
+
+Point pt_curr = new Point();
+Point pt_prev = new Point();
+for (int i = 0; i < n; i++)
+{
+    pt_curr = arr[i];
+    if (i == 0)
+    {
+        Console.WriteLine("처음이다.");
+        pt_prev = arr[i];
+        continue;
+    }
+  
+
+    if (pt_prev.X == pt_curr.X)
+    {
+        // y축으로 이동
+        int min_y = Math.Min(pt_prev.Y, pt_curr.Y);
+        int max_y = Math.Max(pt_prev.Y, pt_curr.Y);
+
+        for (int k = min_y; k < max_y; k++)
+        {
+            arr_y[k + 500000]++;
+        }
+    }
+    else
+    {
+        // x축으로 이동
+        int min_x = Math.Min(pt_prev.X, pt_curr.X);
+        int max_x = Math.Max(pt_prev.X, pt_curr.X);
+
+        for (int k = min_x; k < max_x; k++)
+        {
+            arr_x[k + 500000]++;
+        }
+    }
+    pt_prev = pt_curr;
+}
+
+int max_value = 0;
+for (int i = 0; i < arr_x.Length; i++)
+{
+    if (arr_x[i] == 0) continue;
+    Console.WriteLine(" x[{0}] : {1}", i - 500000, arr_x[i]);
+    if (max_value < arr_x[i])
+    {
+        max_value = arr_x[i];
+        Console.WriteLine("최대값 갱신 {0} -> {1}", max_value, arr_x[i]);
+    }
+}
+for (int i = 0; i < arr_y.Length; i++)
+{
+    if (arr_y[i] == 0) continue;
+    Console.WriteLine(" y[{0}] : {1}", i - 500000, arr_y[i]);
+    if (max_value < arr_y[i])
+    {
+        max_value = arr_y[i];
+        Console.WriteLine("최대값 갱신 {0} -> {1}", max_value, arr_y[i]);
+    }
+}
+
+Console.WriteLine("최대값은 : {0}", max_value);
+
+
+
+#endif
